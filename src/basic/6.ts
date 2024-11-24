988da17541cfe9c1766be7e33eb8d276e198f6b4
edellveis <161501144+edellveis@users.noreply.github.com>
2024-11-24 02:27:33 +0200

    finish-ts-hw

---
+++ src/basic/6.ts
@@ -0,0 +1,17 @@
+interface UserProfile {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const mango: UserProfile = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
+
+const poly: UserProfile = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
